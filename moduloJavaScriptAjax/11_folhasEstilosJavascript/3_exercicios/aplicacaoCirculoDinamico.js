666ffe05140a058df882aa32e2ba94f3ca7eadde
Jaqueline <jgomesribeirolira@gmail.com>
2024-12-23 03:56:22 +0000

    criar circulos azuis, ao passar o cursos fica transparente, e quando o cursor sair deve-se ficar transparente

---
+++ moduloJavaScriptAjax/11_folhasEstilosJavascript/3_exercicios/aplicacaoCirculoDinamico.js
@@ -0,0 +1,11 @@
+function jogada (id){
+    mudarCor(id);
+}
+
+function mudarCor(caixa){
+    caixa.style.backgroundColor = 'rgba(0,0,0,0)';
+}
+
+function reset(caixa){
+    caixa.style.backgroundColor = '';
+}
