5c9ee523604aada9b017e80e5762b26388c71ac0
Jaqueline <jgomesribeirolira@gmail.com>
2025-01-12 13:16:42 +0000

    utilizando a funcao animate, é possivel criar vários efeitos personalizados

---
+++ moduloJavaScriptAjax/13_conceitosBasicosAjaxJsonJquery/2_exemplos/exemplo12/animate.js
@@ -0,0 +1,11 @@
+function animacao1() {
+    $('#caixa1').animate({ opacity: '0.4', width: 500 }, 1000);
+}
+
+function animacao2() {
+    $('#caixa2').animate({ borderWidth: '10px', marginLeft: '120px', width: 500 }, 1000);
+}
+
+function animacao3() {
+    $('#caixa3').animate({ fontSize: '20px' }, 1000);
+}
