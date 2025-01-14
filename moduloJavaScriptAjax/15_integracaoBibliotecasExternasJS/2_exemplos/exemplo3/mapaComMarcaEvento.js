d5a9e1635519153b45e3f647f63985f9b82f26db
Jaqueline <jgomesribeirolira@gmail.com>
2025-01-14 21:54:36 +0000

    exemplo com mcar no mapa do Google com evento ao clicar

---
+++ moduloJavaScriptAjax/15_integracaoBibliotecasExternasJS/2_exemplos/exemplo3/mapaComMarcaEvento.js
@@ -0,0 +1,26 @@
+function carregarMapa() {
+    var ponto = new google.maps.LatLng(38.73376520878317, -9.141150730691137);
+
+    var opcoes = {
+        zoom: 12,
+        center: ponto,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var m = new google.maps.Map(document.getElementById("mapa"), opcoes);
+
+    var marca = new google.maps.Marker({
+        position: ponto,
+        map: m,
+    });
+
+    var caixa = new google.maps.InfoWindow({
+        content: 'Empresa: <b>MasterD</b> <br/> Visite-nos!'
+    });
+
+    google.maps.event.addListener(
+        marca, 'click', function () {
+            caixa.open(m, marca);
+ });
+
+}
