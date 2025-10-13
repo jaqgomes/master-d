9068b28a19f541cfadb41ab9ad10fe3e4856a437
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-14 00:39:11 +0100

    exercio itroducao php e pratica 26 php

---
+++ moduloPhp/1-introdPhp/4_pratica26/WDL_praticas/pratica1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prática 26</title>
+    <meta name="author" content="Jaqueline Lima" />
+
+</head>
+
+<body>
+    <?php
+
+    echo "Olá mundo! <br>";
+    echo "Olá mundo! <br>";
+    echo "Olá mundo! <br>";
+
+    $cor = "azul";
+    $COR = "amarelo";
+    $Cor = "azul claro";
+
+    echo "O meu carro é " . $cor . "<br>";
+    echo "A minha cor favorita é " . $COR . "<br>";
+    echo "O céu é " . $Cor . "<br>";
+    ?>
+</body>
+
+</html>
