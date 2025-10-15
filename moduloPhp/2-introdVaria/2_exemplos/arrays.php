a53fa2efe0d1a56e35f6237675109ac348ef9c38
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-15 17:42:21 +0100

    exemplo array em php

---
+++ moduloPhp/2-introdVaria/2_exemplos/arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+
+        <title>Arrays php</title>
+</head>
+<body>
+    <?php
+    $vector = array("Idade" => 12, 20 => true);
+
+    echo $vector["Idade"]; //mostra 12
+    echo $vector[20]; //mostra 1
+
+    ?>
+</body>
+</html>
