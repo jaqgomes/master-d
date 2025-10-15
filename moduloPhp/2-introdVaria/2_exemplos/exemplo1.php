10bd81134193c425644e054c1d4784e0e1f765d5
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-15 17:21:48 +0100

    php string

---
+++ moduloPhp/2-introdVaria/2_exemplos/exemplo1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+
+    <title>String php</title>
+</head>
+
+<body>
+
+
+    <?php
+    $variavel = "isto é um teste";
+
+    echo $variavel[2]; /* mostrará `t`*/
+    ?>
+
+
+
+</body>
+
+</html>
