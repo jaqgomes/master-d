0ee7e833492325c2f8ee9fbf32350f3078b35d1b
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-17 17:45:08 +0100

    remover array em php

---
+++ moduloPhp/2-introdVaria/2_exemplos/removerArray.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+
+    <title>Remover array</title>
+</head>
+<body>
+    <?php
+    //para remover um elemento em array, é utilizado o unset e a chave a eliminar.
+    //unset($vector["idade"]);
+
+    //para remover um array inteiro, é utilizado o unset mas com apenas o nome do array.
+    //unset($vector);
+    ?>
+</body>
+</html>
