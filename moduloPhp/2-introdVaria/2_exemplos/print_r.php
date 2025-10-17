b779fef60a58f18c7786053bcc0159f9cb9a9b43
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-17 17:58:24 +0100

    exibir todos os dados em array

---
+++ moduloPhp/2-introdVaria/2_exemplos/print_r.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Exibir todos os dados em array</title>
+</head>
+
+<body>
+    <?php
+
+    $a = array('nome' => 'Pedro', 'Idade' => '34', 'carros' => array('renault', 'seat', 'ford'));
+    print_r($a);
+    ?>
+</body>
+
+</html>
