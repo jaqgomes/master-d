5ad035dcf18842eb7ef4df555eb7834dd87b9121
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-22 19:33:42 +0100

    exemplo de metodo Post

---
+++ moduloPhp/2-introdVaria/2_exemplos/metodoPost.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+    <meta name="description" content="exemplo de variavel metodo Post">
+
+    <title>Metodo Post</title>
+</head>
+
+<body>
+    <p>
+
+    <form action="exemplo.php" method="post">
+        Nome:<input type="text" name="nome"/>
+
+        Email: <input type="text" name="email"/>
+
+        <input type="submit" name="submit" value="enviar">
+
+
+    </form>
+
+
+    </p>
+
+</body>
+
+</html>
