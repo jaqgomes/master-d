997af8965696d2bb3aa941c894b6b36b0d5dcb0d
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-10-27 10:25:52 +0000

    exercicio de arrays compostos por dados de exibição de valores com recurso a estrutura de controlo

---
+++ moduloPhp/3-estruturaPhp/3_exercicio/exercCriarArray.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+    <meta name="description"
+        content="Exercicio de arrays compostos por dados de exibição de valores com recurso a estrutura de controlo">
+
+    <title>Exercício de Arrays</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $cores = array("laranja", "azul", "verde", "amarelo", "vermelho", "roxo");
+
+        foreach ($cores as $var) {
+
+            echo "Cor: $var <br>\n";
+        }
+        ?>
+    </p>
+</body>
+
+</html>
