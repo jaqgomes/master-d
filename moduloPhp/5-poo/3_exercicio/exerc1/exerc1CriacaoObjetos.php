63635e81a1c07a0e6be704c6f2ddddd21d8cade8
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-12-07 22:12:11 +0000

    Adjusting to working on other computer

---
+++ moduloPhp/5-poo/3_exercicio/exerc1/exerc1CriacaoObjetos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaqueline Lima">
+    <meta name="description" content="exercicio de criação de objetos do tipo classe devolução dos dados">
+
+    <title>Exercício criação de objetos do tipo classe devolução dos dados </title>
+</head>
+
+<body>
+    <?php
+        class equipa{
+            public $nome;
+            public $perdidos;
+            public $ganhos;
+
+            public function __construct($nome, $ganhos, $perdidos){
+                
+            }
+        }
+    ?>
+</body>
+
+</html>
