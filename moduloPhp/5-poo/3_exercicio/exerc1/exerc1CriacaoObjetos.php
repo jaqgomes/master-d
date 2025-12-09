87e811dbf2af53e460f91f8ca1cdbaf30b6f08e3
Jaqueline Lima <jgomesribeirolira@gmail.com>
2025-12-09 19:17:04 +0000

    exercicio criacao e objetos do tipo classe e devolucao dos seus dados

--- moduloPhp/5-poo/3_exercicio/exerc1/exerc1CriacaoObjetos.php
+++ moduloPhp/5-poo/3_exercicio/exerc1/exerc1CriacaoObjetos.php
@@ -12,15 +12,68 @@
 
 <body>
     <?php
-        class equipa{
-            public $nome;
-            public $perdidos;
-            public $ganhos;
+    class equipa
+    {
+        public $nome;
+        public $perdidos;
+        public $ganhos;
 
-            public function __construct($nome, $ganhos, $perdidos){
-                
-            }
+        public function __construct($nome, $ganhos, $perdidos)
+        {
+            $this->nome = $nome;
+            $this->ganhos = $ganhos;
+            $this->perdidos = $perdidos;
         }
+
+        public function lerNome()
+        {
+            return $this->nome;
+
+        }
+        public function lerGanhos()
+        {
+            return $this->ganhos;
+        }
+
+        public function lerPerdidos()
+        {
+            return $this->perdidos;
+        }
+
+        public function qualidade()
+        {
+            return (($this->ganhos * 100) / ($this->ganhos + $this->perdidos));
+        }
+
+        public function somarVitoria()
+        {
+            $this->ganhos++;
+
+        }
+
+        public function somarDerrota()
+        {
+            $this->perdidos++;
+        }
+    }
+
+    $obj = new equipa('Lisboa', 5, 5);
+    $obj2 = new equipa('Porto', 2, 7);
+
+    $obj->somarVitoria();
+    $obj->somarVitoria();
+
+    $obj2->somarVitoria();
+    $obj->somarDerrota();
+
+    echo $obj->lerNome() . ' com ' . $obj->lerGanhos() . ' jogos ganhos e 
+    ' . $obj->lerPerdidos() . ' perdidos, tem uma qualidade média de 
+    ' . $obj->qualidade() . '</br>';
+
+    echo $obj2->lerNome() . ' com ' . $obj2->lerGanhos() . ' jogos ganhos e 
+    ' . $obj2->lerPerdidos() . ' perdidos, tem uma qualidade média de 
+    ' . $obj2->qualidade() . '</br>';
+
     ?>
 </body>
 
