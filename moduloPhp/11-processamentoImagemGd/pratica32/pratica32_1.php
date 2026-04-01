afd8d0ffcfe8cde806e2e1ccafff7733a247fcd9
Jaqueline Lima <jgomesribeirolira@gmail.com>
2026-04-01 23:49:16 +0100

    pratica 32 - edicao de imagens com php atraves da biblioteca GD

---
+++ moduloPhp/11-processamentoImagemGd/pratica32/pratica32_1.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Prática 32</title>
+    <link href="estilos.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="caixa0">
+        <span id="logo"><img src="logo.png"></span>
+    </div>
+    <div class="caixa1">
+
+        <img src="php.png" style="width: 100%;">
+
+        <?php
+
+        $image = imagecreatefrompng('php.png');
+
+        $stamp = imagecreatefrompng('logo.png');
+
+        $right = 350;
+        $bottom = 50;
+        $sx = imagesx($stamp);
+        $sy = imagesy($stamp);
+
+        imagecopy($image, $stamp, imagesx($image) - $sx - $right, imagesy($image) - $sy - $bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+        imagepng($image, 'php.png');
+
+        imagedestroy($image);
+
+        ?>
+    </div>
+</body>
+
+</html>
